410741277f72466525d6246930c62f8fc970d507
AlexGrizlie <32987699+AlexGrizlie@users.noreply.github.com>
2017-10-22 01:19:09 +0300

    Add files via upload

---
+++ DZ/DZ_1/script.js
@@ -0,0 +1,27 @@
+var user={
+ name: 'Alexandr',
+    lastName: 'Grigorenko',
+    sayHello: function (){
+     console.log('hello');
+    },
+    goToCar: function (){
+        car['user'] = user;
+        }
+};
+var car = {
+    model: 'lanos'
+}
+   user.goToCar();
+    console.log(car);
+
+var x=1;
+function test1(){
+    var y=2; 
+    console.log(y);
+}
+function test(){
+    console.log(x);
+}
+test1();
+test();
+        
